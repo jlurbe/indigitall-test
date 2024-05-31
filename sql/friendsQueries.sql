81debba5029a4f7d6e949038578766bada06ba40
JuanJo Lurbe <juanjose.lurbe@garajedeideas.com>
2024-05-31 09:59:28 +0200

    fix: removed innecesary join

--- sql/friendsQueries.sql
+++ sql/friendsQueries.sql
@@ -5,6 +5,6 @@ INNER JOIN friendships f ON f.friend_id=u.id
 WHERE f.user_id = 1
 
 -- Número de amigos del usuario con id 1
-SELECT count(*) as num_friends FROM users u
-INNER JOIN friendships f ON f.friend_id=u.id
+SELECT count(*) as num_friends 
+FROM friendships f
 WHERE f.user_id = 1
